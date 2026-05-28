d941a10c2115b30f77019e8d38e33de008a00ce2
G D NAGA PRASANNA <228t1a4240@diet.ac.in>
2026-05-28 19:31:05 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,33 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name="Deekshith"
+>>> print(name)
+Deekshith
+>>> Name="Codegnan"
+>>> print(Name)
+Codegnan
+>>> x="Deekshith"
+>>> y="Goda"
+>>> print(x,y)
+Deekshith Goda
+>>> _0=9
+>>> print(_0)
+9
+>>> NAME="DHANEKULA"
+>>> print(NAME)
+DHANEKULA
+>>> fname="Goda"
+>>> lname="Deekshith"
+>>> print(fname+" "+lname)
+Goda Deekshith
+>>> #Variable never start with Spaces & numbers & Special Characters
+>>>  x="Deekshith"
+...  
+SyntaxError: unexpected indent
+>>> @="Codegnan"
+SyntaxError: invalid syntax
+>>> 
+>>> 9="Dhanekula"
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> 9 = "Dhanekula"
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
