34aa275a09c8c7c6a40627662d454a5a4da66d4a
G D NAGA PRASANNA <228t1a4240@diet.ac.in>
2026-06-09 20:01:46 +0530

    Add files via upload

---
+++ practiced_programs.py
@@ -0,0 +1,141 @@
+'''age =int( input("Enter the age of the person:"))
+if age>=18:
+    print("Eligible for vote")
+else:
+    print("Not Eligible for vote")'''
+
+
+
+
+'''num = int(input("Enter the number:"))
+if num %2==0:
+    print("It is even")
+else:
+    print("It is odd")'''
+
+
+
+
+
+'''leap_year=int(input("Enter the year"))
+if leap_year%4==0:
+    print("Leap year")
+else:
+    print("Not leap year")'''
+
+
+'''a = input("Enter the letter:")
+if a in "aeiouAEIOU":
+    print("Vowels")
+else:
+    print("Consenets")'''
+
+'''a=input("Enter the name:")
+if a.lower()  == "deekshith":
+    print("Welcome Deekshith")
+else:
+    print("Welcome Guest")'''
+
+'''a=input("Enter the name:")
+b=input("Enter the name:")
+c=input("Enter the name:")
+d=input("Enter the name:")
+if a.lower()  == "deekshith":
+    print("Welcome Deekshith")
+    
+    if a.lower()  == "karthik":
+        print("Welcome Karthik")
+        
+        if b.lower()  == "Nani":
+            print("Welcome Nani")
+            
+             if c.lower()  == "Chandra":
+                 print("Welcome Chandra")
+            
+else:
+    print("Welcome Guest")'''
+
+
+'''name=["dinesh","Prince","hari","vamsi","sravanthi"]
+name1=input("Enter the name:")
+if name in name1:
+    print("Welcome Dinesh")
+else:
+    print("Welcome guest")'''
+
+
+'''name =input("Enter the Id name:")
+password=int(input("Enter the Password"))
+if name == "admin":
+    if password ==1234:
+        print("login  Succesfully")
+    else:
+        print("Wrong password")
+else:
+    print("invalid credentials")'''
+
+
+'''username=input("enter the username")
+password=int(input("Enter the password"))
+if username=="deekshith" and password==1234:
+    print("login successfull")
+else:
+    print("invalid Credentials")'''
+
+'''username=input("enter the username")
+password=input("Enter the password")
+if username=="deekshith" and password=="deekshith@123":
+    print("login successfull")
+else:
+    print("invalid Credentials")'''
+
+
+'''cake=input("Enter the cake")
+if cake =="Redvelvet cake":
+    print("1200")
+elif cake =="Almode cake":
+    print("1000")
+elif cake =="Butter scotch":
+    print("800")
+elif cake =="Chocklate":
+    print("800")
+else:
+    print("cake is not available")'''
+
+
+'''pizza=int(input("Enter the cake"))
+if pizza == 1200:
+    print("Cripy chiecken")
+elif pizza ==1000:
+    print("pizza")
+elif pizza==200:
+    print("Chicken puff")
+elif pizza ==900:
+    print("Leg pieases")
+else:
+    print("items is not available")'''
+
+
+age=int(input("Enter the age:"))
+marks=int(input("Enter the marks:"))
+attendence=int(input("Enter the attendence:"))
+if age >= 18:
+    print("they are Eligible for vote")
+if marks >=80:
+    print("Eligible for write exmas")
+if attendence >=90:
+    print("Eligible for schorlship")
+            
+
+
+
+
+    
+
+
+
+
+
+
+
+        
