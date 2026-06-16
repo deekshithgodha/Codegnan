183fa8b21a53225ee3727eb1ccabe8bcc83da316
G D NAGA PRASANNA <228t1a4240@diet.ac.in>
2026-06-16 19:44:43 +0530

    Add files via upload

---
+++ triangels.py
@@ -0,0 +1,31 @@
+'''n=int(input("Enter the rows"))
+for i in range(1,n+1):
+    print(" * " * i)'''
+
+'''for i in range(1,6):
+    print(" * " * i)'''
+
+'''n = int(input("Enter the rows: "))
+for i in range(n, 0, -1):
+    print(" * " * i)'''
+
+n = int(input("Enter the rows: "))
+for i in range(n):
+    print(" * " *n)
+    
+'''n=int(input("Enter the rows"))  
+for i in range(n):
+    print(" * " * n)'''
+
+
+'''rows=4
+for i in range(1,rows+1):
+    print("  "*(rows - i)+" * "*i)'''
+
+'''while True:
+    rows=int(input("Enter the rows"))
+    for i in range(1,rows+1):
+        print(" " * (rows - i)+"*" * i)'''
+
+
+
